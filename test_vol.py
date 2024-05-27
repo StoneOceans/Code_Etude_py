dbe9bad5a14ae5089bc0bed9e7b1d63178e183ba
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-05-27 14:44:00 +0200

    Update test_vol.py

--- test_vol.py
+++ test_vol.py
@@ -1,3 +1,5 @@
+# test_vol.py
+
 import pytest
 from src.mains import parse_pln_file
 import pandas as pd
@@ -14,7 +16,7 @@ def test_parse_pln_file():
     21 -525 350 485 -525
     22 I S AA47735238 0 0 FPL 21052023 SANDY 0 0 21-05-2023 00:00
     23 0 0 0 ??????
-    24  -1 ???????? 0
+    24  -1 ???????? 0
     2R LASAT390
     31 SANDY MOTOX RUCAC LESDO RANUX ETINO NEBAX LASAT DEVDI 
     32 -105 -162 -161 -149 -142 -138 -131 -128 -64 
@@ -28,7 +30,7 @@ def test_parse_pln_file():
     21 -525 350 485 -525
     22 I S AA47735238 0 0 FPL 21052023 SANDY 0 0 21-05-2023 00:00
     23 0 0 0 ??????
-    24  -1 ???????? 0
+    24  -1 ???????? 0
     2R LASAT390
     31 SANDY MOTOX RUCAC LESDO RANUX ETINO NEBAX LASAT DEVDI 
     32 -113 -113 -112 -100 -93 -89 -82 -79 -72 
@@ -38,11 +40,11 @@ def test_parse_pln_file():
     71 EGGG REIM ZURI 
     72 1 2 5 
     12
-    20 ETH575 KORD HAAB  -1 -1 B788 0000 ETAOU 0
+    20 ETH575 KORD HAAB  -1 -1 B788 0000 ETAOU 0
     21 -525 390 485 -525
     22 I S AA47735238 1 0 FPL 21052023 MOTOX 1 1328 21-05-2023 00:00
     23 0 0 0 ??????
-    24  -1 ???????? 0
+    24  -1 ???????? 0
     2R ??
     31 MOTOX PTGEO PTGEO PTGEO PTGEO PTGEO PTGEO PTGEO 
     32 -112 -112 -112 -101 -90 -83 -78 
@@ -58,6 +60,7 @@ def test_parse_pln_file():
     83 ETH575 LETAOUI 1B788479 370             212152F    S KORDHAABYW
     84 ETH575 LETAOUI 1B788480 370     370     212207SANDYS KORDHAABYW
     """
+    
     # Write the sample content to a temporary file
     with open('sample_test_file.pln', 'w') as file:
         file.write(sample_content)
@@ -65,41 +68,40 @@ def test_parse_pln_file():
     # Call the function with the temporary file
     output_df = parse_pln_file('sample_test_file.pln')
 
-    # Check the expected values in the dataframe
+    # Create the expected dataframe manually (you might need to adjust this based on your actual expected output)
     expected_data = {
-        'callsignprevu': ['AA1234'],
-        'depprevu': ['DEP'],
-        'arrprevu': ['ARR'],
-        'numcautraprevu': ['1234'],
-        'typeavionprevu': ['A320'],
-        'workprevu': ['WORK'],
-        'work1prevu': [''],
-        'heuresdedepprevu': ['1200'],
+        'callsignprevu': ['ETH575'],
+        'depprevu': ['KORD'],
+        'arrprevu': ['HAAB'],
+        'numcautraprevu': ['9273'],
+        'typeavionprevu': ['B788'],
+        'workprevu': ['ETAOU'],
+        'heuresdedepprevu': ['-525'],
         'RFLprevu': ['350'],
-        'vitesseprevu': ['500'],
-        'EOBTprevu': ['EOBT'],
-        'regledevolprevu': ['REG1'],
-        'typedevolprevu': ['TYPE1'],
-        'IFPLprevu': [''],
-        'PLN_activeprevu': ['1'],
+        'vitesseprevu': ['485'],
+        'EOBTprevu': ['-525'],
+        'regledevolprevu': ['I'],
+        'typedevolprevu': ['S'],
+        'IFPLprevu': ['AA47735238'],
+        'PLN_activeprevu': ['0'],
         'PLN_annuleprevu': ['0'],
-        'date_blockprevu': ['22052023'],
-        'adressemodeprevu': ['ADRSMODE'],
-        'numeroPLNMprevu': ['PLN123'],
-        'FlightIDprevu': ['FlightID'],
-        'baliseprevu': ['BALISE'],
-        'listhourprevu': ['1200'],
-        'listedesbalistesprevu': ['BALISTES'],
-        'indicateurprevu': ['INDICATEUR'],
-        'carteprevu': ['CARTE'],
-        'centretraverséprevu': ['CENTRE'],
-        'listederangpremierprevu': ['RANG'],
-        'rangtransactionprevu': ['TRANSACTION'],
-        'heure': ['12'],
-        'minute': ['00'],
-        'accusetrtprevu': ['1200'],
-        'ccr_arrival': ['ARR'],
-        'finalprevu': ['FINAL']
+        'date_blockprevu': ['21052023'],
+        'adressemodeprevu': [pd.NA],
+        'numeroPLNMprevu': [pd.NA],
+        'FlightIDprevu': [pd.NA],
+        'baliseprevu': ['SANDY'],
+        'listhourprevu': ['-105'],
+        'listedesbalistesprevu': ['370'],
+        'indicateurprevu': ['0'],
+        'carteprevu': ['HN'],
+        'centretraverséprevu': ['EGGG'],
+        'listederangpremierprevu': ['1'],
+        'rangtransactionprevu': [pd.NA],
+        'heure': [pd.NA],
+        'minute': [pd.NA],
+        'accusetrtprevu': [pd.NA],
+        'ccr_arrival': [pd.NA],
+        'finalprevu': [pd.NA]
     }
 
     expected_df = pd.DataFrame(expected_data)
