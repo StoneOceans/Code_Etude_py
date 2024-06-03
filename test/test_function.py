43e012cadc8c330626c509d3b1dfaf133e25d7a1
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-03 13:05:16 +0200

    Update test_function.py

--- test/test_function.py
+++ test/test_function.py
@@ -1,8 +1,14 @@
-# src/analyse.py
+from datetime import datetime
 import pandas as pd
 import numpy as np
-from datetime import datetime
-vol_prevu = []
+import unittest
+
+
+class TestVolData(unittest.TestCase):
+
+    def setUp():
+        # This method will be used to set up the DataFrame for testing
+        vol_prevu = []
 vol_fini = []
 vol_termine = []
 tableau_vol={}
@@ -19,7 +25,7 @@ complet = 0
 output = pd.DataFrame()
 compteur = 0
 
-with open('/content/drive/MyDrive/Colab Notebooks/RDVC-20230522.pln', 'r') as fichier:
+with open('/home/ajar/Téléchargements/TEst/RDVC-20230522.pln', 'r') as fichier:
 #with open('/content/drive/MyDrive/DSNA/Redevances/RDVC-20230522.pln', 'r') as fichier:
     for i, ligne in enumerate(fichier):
         words = ligne.split()
@@ -29,9 +35,16 @@ with open('/content/drive/MyDrive/Colab Notebooks/RDVC-20230522.pln', 'r') as fi
           date_fichier = date_obj.timetuple().tm_yday
         if words[0] == "05":
           if tableau_vol:
+            tableau_vol["isPrevu"]= isprevu
+            tableau_vol["isRealise"] = isrealise
+            tableau_vol["isFinal"] = isfinal
             #output = pd.concat([output, pd.DataFrame.from_dict(tableau_vol)], ignore_index=True)
             df_dictionary = pd.DataFrame([tableau_vol])
             output = pd.concat([output, df_dictionary], ignore_index=True)
+          tableau_vol = {}
+          isprevu = False
+          isrealise= False
+          isfinal= False
 
 
           balise = ''
@@ -40,11 +53,19 @@ with open('/content/drive/MyDrive/Colab Notebooks/RDVC-20230522.pln', 'r') as fi
           etat = ''
         if words[0] == "11":
                 etat='prevu'
+                isprevu = True
         if words[0] == "12":
-                etat = 'final'
+                etat = 'realise'
+                isrealise= True
 
         if words[0] == "13":
-                etat='realise'
+             if len(words) > 1 and words[1] == "=":
+                for key in list(tableau_vol.keys()):
+                    if '_prevu' in key:
+                        tableau_vol[key.replace('_prevu', '_final')] = tableau_vol[key]
+             etat='final'
+             isfinal= True
+
 
         if words[0] == "14":
                 etat='transaction'
@@ -54,28 +75,36 @@ with open('/content/drive/MyDrive/Colab Notebooks/RDVC-20230522.pln', 'r') as fi
 
 
         if words[0] == "20":
-                tableau_vol['callsign_'+etat]= words[1]
+                tableau_vol['callSign_'+etat]= words[1]
                 tableau_vol['dep_'+etat]= words[2]
                 tableau_vol['arr_'+etat]= words[3]
-                tableau_vol['numcautra_'+etat]= words[4]
-                tableau_vol['typeavion_'+etat]= words[6]
-
+                tableau_vol['numCautra_'+etat]= words[4]
+                tableau_vol['dateRelative_'+etat]= words[5]
+                tableau_vol['typeAvion_'+etat]= words[6]
+                tableau_vol['work_'+etat]= words[7]
+                if words[8][:2] == '??':
+                  #tableau_vol['work1'+etat] = f"{' ' * 9}"
+                  pass
+                else:
+                  tableau_vol['work1'+etat] = words[8].strip().ljust(9)
 
 
         if words[0] == "21":
-
+                tableau_vol['heuresDep_'+etat]= words[1]
+                tableau_vol['RFL_'+etat]= words[2]
                 tableau_vol['vitesse_'+etat]= words[3]
                 tableau_vol['EOBT_'+etat]= words[4]
         if words[0] == "22":
-                tableau_vol['regledevol_'+etat]= words[1]
-                tableau_vol['typedevol_'+etat]= words[2]
+                tableau_vol['regleVol_'+etat]= words[1]
+                tableau_vol['typeVol_'+etat]= words[2]
+                tableau_vol['HeurePremiereBaliseActive_'+etat] = words[10]
                 if words[3][:2] == '??':
                   #tableau_vol['IFPL'+etat] = f"{' ' * 10}"
                   pass
                 else:
                   tableau_vol['IFPL_'+etat] = words[3].strip().ljust(10)
-                tableau_vol['PLN_active_'+etat]= words[4]
-                tableau_vol['PLN_annule_'+etat]= words[5]
+                tableau_vol['plnActive_'+etat]= words[4]
+                tableau_vol['plnAnnule_'+etat]= words[5]
                 if '??' in words[7]:
                   #tableau_vol['date_block'+etat] = f"{' ' * 6}"
                   pass
@@ -92,40 +121,42 @@ with open('/content/drive/MyDrive/Colab Notebooks/RDVC-20230522.pln', 'r') as fi
                   #  prevu = False
                   #  termine = False
                   #  final = False
-                  tableau_vol['date_block_'+etat] = words[7][:4] + words[7][6:]
+                  tableau_vol['dateBlock_'+etat] = words[7][:4] + words[7][6:]
                 else:
-                  tableau_vol['date_block_'+etat] = words[7].strip().ljust(6)
+                  tableau_vol['dateBlock_'+etat] = words[7].strip().ljust(6)
 
 
 
         if words[0] == "23":
             if "??" in words[4]:
-                tableau_vol['adressemode_'+etat]= np.NaN
+                tableau_vol['adresseModeS_'+etat]= np.NaN
             else:
-                tableau_vol['adressemode_'+etat]= words[4]
+                tableau_vol['adresseModeS_'+etat]= words[4]
         if words[0] == "24":
-                tableau_vol['numeroPLNM_'+etat]= words[1]
-                tableau_vol['FlightID_'+etat]= words[2]
+                tableau_vol['numeroPLNM'+etat]= words[1]
+                tableau_vol['flightID'+etat]= words[2]
         if words[0] == "31":
-                tableau_vol['balise_'+etat]= words[1]
+                tableau_vol['balise'+etat]= words[1]
         if words[0] == "32":
-                tableau_vol['listhour_'+etat]= words[1]
+                tableau_vol['HeurePremiereBalise_'+etat]= words[1]
         if words[0] == "33":
-                tableau_vol['listedesbalistes_'+etat]= words[1]
+                tableau_vol['listeBalises'+etat]= words[1]
         if words[0] == "36":
-                tableau_vol['indicateur_'+etat]= words[1]
+                tableau_vol['indicateur'+etat]= words[1]
         if words[0] == "41":
-                tableau_vol['carte_'+etat]= words[1]
+                tableau_vol['carte'+etat]= words[1]
         if words[0] == "71":
-                tableau_vol['centretraversé_'+etat]= words[1]
+                tableau_vol['centreTraversé'+etat]= words[1]
         if words[0] == "72":
-                tableau_vol['listederangpremier_'+etat]= words[1]
+                tableau_vol['listeRangPremier'+etat]= words[1]
         if words[0] == "80":
-                tableau_vol['rangtransaction_'+etat]= words[1]
+                tableau_vol['rangTransaction'+etat]= words[1]
         if words[0] == "81":
             if len(words) >= 15:
                 parts = ligne.split("-")
                 last_word = parts[0].split()[-1]
+                if "ABI" in ligne:
+                  tableau_vol['typePln']= "ABI"
                 if "(FPL" in parts[0] or "(CHG)" in parts[0]:
                   tableau_vol['case7']= parts[1]
                   tableau_vol['case8']= parts[2]
@@ -136,10 +167,8 @@ with open('/content/drive/MyDrive/Colab Notebooks/RDVC-20230522.pln', 'r') as fi
                   if len(parts)>8:
                     tableau_vol['case16']= parts[7]
                     tableau_vol['case18']= parts[8]
-                  elif tableau_vol['case18']= "RPL":
-                    tableau_vol['typePLN']="RPL"
-                  
-
+                    if tableau_vol['case18']== "RPL":
+                      tableau_vol['typePln']="RPL"
                   else:
                     print(ligne)
                     compteur += 1
@@ -150,6 +179,7 @@ with open('/content/drive/MyDrive/Colab Notebooks/RDVC-20230522.pln', 'r') as fi
                   tableau_vol['case10']= parts[4]
                   tableau_vol['case13']= parts[5]
                   tableau_vol['case15']= parts[6]
+                  tableau_vol['typePln']= "APL"
                   if len(parts)>8:
                     tableau_vol['case16']= parts[7]
                     tableau_vol['case18']= parts[-1]
@@ -167,45 +197,35 @@ with open('/content/drive/MyDrive/Colab Notebooks/RDVC-20230522.pln', 'r') as fi
         if words[0] == "82":
                 tableau_vol['heure'] = (words[1][:2])
                 tableau_vol['minute'] = (words[1][3:])
-                tableau_vol['accusetrt'+etat]= words[1]
+                tableau_vol['accuseTrt'+etat]= words[1]
                 if "CCR:" in ligne:
-                  compteur_CCr = 0
+                  compteurCcr = 0
                   for word in words:
-                    compteur_CCr += 1
+                    compteurCcr += 1
                     if word == "CCR:":
                       break
-                  tableau_vol['ccr_arrival'] = words[compteur_CCr]
+                  tableau_vol['ccrArrival'] = words[compteurCcr]
         if words[0] == "84":
-                tableau_vol['final_'+etat]= words[1]
+                tableau_vol['final'+etat]= words[1]
 
+        
 
+    def test_same_final_and_prevu_LRQ267G(self):
+        # Rule: LRQ267G should have the same values for 'final' and 'prevu'
+        lrq267g =output[output['callSign_prevu'] == 'LRQ267G']
+        output.assertTrue(lrq267g['isFinal'].iloc[0] == lrq267g['isPrevu'].iloc[0])
 
+    def test_heure_de_reference_EZY37KC(self):
+        # Rule: EZY37KC should have an heure_de_reference equal to a float of -1440
+        ezy37kc =output[output['callSign_prevu'] == 'EZY37KC']
+        output.assertEqual(ezy37kc['heure_de_reference'].iloc[0], -1440.0)
 
+    def test_heure_de_reference_TRA79Y(self):
+        # Rule: TRA79Y should have an heure_de_reference equal to a float of 1720
+        tra79y =output[output['callSign_prevu'] == 'TRA79Y']
+        output.assertEqual(tra79y['heure_de_reference'].iloc[0], 1720.0)
 
+if __name__ == '__main__':
+    unittest.main()
 
-
-
-
-
-print(output)
-#vol['time'] = f"{heure:02d}{minute:02d}"
-def load_data(file_path):
-    tableau_vol = {}
-    output = pd.DataFrame()
-    with open(file_path, 'r') as fichier:
-        for i, ligne in enumerate(fichier):
-            words = ligne.split()
-            # Process the lines and populate tableau_vol
-            # This is just a simplified placeholder for demonstration
-            if words[0] == "20":
-                tableau_vol['callsign_realise'] = words[1]
-                tableau_vol['date_block_realise'] = '22052023'  # Example date
-                tableau_vol['date_relative_realise'] = -1  # Example relative day
-                tableau_vol['listhour_realise'] = '1230'  # Example time
-    return tableau_vol
-
-# Example usage of the load_data function
-if __name__ == "__main__":
-    tableau_vol = load_data('/path/to/your/file')
-    print(tableau_vol)
 
