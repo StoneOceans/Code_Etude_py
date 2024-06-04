0e28bd99cc513e0e12d623b292be42e158682b21
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-04 15:30:02 +0200

    Update test_function.py

--- test/test_function.py
+++ test/test_function.py
@@ -9,8 +9,10 @@ def test_heure_de_reference():
     # Assuming filter_and_analyze has been applied and output DataFrame is filtered
     heure_de_reference1 = output.loc[output['callSign_prevu'] == 'EIN545', 'heure_de_reference'].values[0]
     heure_de_reference_1=output.loc[output['callSign_prevu'] == 'TRA79Y', 'heure_de_reference'].values[0]
+    heure_de_reference0=output.loc[output['callSign_prevu'] == '160B', 'heure_de_reference'].values[0]
     assert heure_de_reference1 == 902.0, "heure_de_reference for EIN545 is not equal to 902"
     assert heure_de_reference_1 == 1720.0, "heure_de_reference for EIN545 is not equal to 902"
+    assert heure_de_reference0 == 0.0, "heure_de_reference for EIN545 is not equal to 902"
 
 
 if __name__ == "__main__":
