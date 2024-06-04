b147a70005608d8cb7a16dc3729aeee05fd05745
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-04 22:39:35 +0200

    Update test_function.py

--- test/test_function.py
+++ test/test_function.py
@@ -14,17 +14,60 @@ def test_heure_de_reference():
     assert heure_de_reference1 == 902.0, "heure_de_reference for EIN545 is not equal to 902"
     assert heure_de_reference_1 == 1720.0, "heure_de_reference for EIN545 is not equal to 902"
     assert heure_de_reference0 == 0.0, "heure_de_reference for EIN545 is not equal to 902"
+    
+def calcul_HeureDeReference(row):
+    try:
+        if not pd.isna(row['dateRelative_realise']) and not pd.isnull(row['dateRelative_realise']):
+            if not pd.isna(row['HeurePremiereBaliseActive_realise']) and not pd.isnull(row['HeurePremiereBaliseActive_realise']):
+                if row['dateRelative_realise'] == 0:
+                    return int(row['HeurePremiereBaliseActive_realise'])
+                elif row['dateRelative_realise'] == 1:
+                    return int(row['HeurePremiereBaliseActive_realise']) - 1440
+                elif row['dateRelative_realise'] == -1:
+                    return int(row['HeurePremiereBaliseActive_realise']) + 1440
+            elif not pd.isna(row['HeurePremiereBaliseActive_final']) and not pd.isnull(row['HeurePremiereBaliseActive_final']):
+                if row['dateRelative_realise'] == 0:
+                    return int(row['HeurePremiereBaliseActive_final'])
+                elif row['dateRelative_realise'] == 1:
+                    return int(row['HeurePremiereBaliseActive_final']) - 1440
+                elif row['dateRelative_realise'] == -1:
+                    return int(row['HeurePremiereBaliseActive_final']) + 1440
+            elif not pd.isna(row['HeurePremiereBalise_final']) and not pd.isnull(row['HeurePremiereBalise_final']):
+                if row['dateRelative_realise'] == 0:
+                    return int(row['HeurePremiereBalise_final'])
+                elif row['dateRelative_realise'] == 1:
+                    return int(row['HeurePremiereBalise_final']) - 1440
+                elif row['dateRelative_realise'] == -1:
+                    return int(row['HeurePremiereBalise_final']) + 1440
+        elif not pd.isna(row['dateRelative_final']) and not pd.isnull(row['dateRelative_final']):
+            if not pd.isna(row['HeurePremiereBaliseActive_realise']) and not pd.isnull(row['HeurePremiereBaliseActive_realise']):
+                if row['dateRelative_final'] == 0:
+                    return int(row['HeurePremiereBaliseActive_realise'])
+                elif row['dateRelative_final'] == 1:
+                    return int(row['HeurePremiereBaliseActive_realise']) - 1440
+                elif row['dateRelative_final'] == -1:
+                    return int(row['HeurePremiereBaliseActive_realise']) + 1440
+            elif not pd.isna(row['HeurePremiereBaliseActive_final']) and not pd.isnull(row['HeurePremiereBaliseActive_final']):
+                if row['dateRelative_final'] == 0:
+                    return int(row['HeurePremiereBaliseActive_final'])
+                elif row['dateRelative_final'] == 1:
+                    return int(row['HeurePremiereBaliseActive_final']) - 1440
+                elif row['dateRelative_final'] == -1:
+                    return int(row['HeurePremiereBaliseActive_final']) + 1440
+            elif not pd.isna(row['HeurePremiereBalise_final']) and not pd.isnull(row['HeurePremiereBalise_final']):
+                if row['dateRelative_final'] == 0:
+                    return int(row['HeurePremiereBalise_final'])
+                elif row['dateRelative_final'] == 1:
+                    return int(row['HeurePremiereBalise_final']) - 1440
+                elif row['dateRelative_final'] == -1:
+                    return int(row['HeurePremiereBalise_final']) + 1440
+    except Exception as e:
+        return None  # Handle any exceptions gracefully
 
 def test_calcul_HeureDeReference():
-    data = {
-        'dateRelative_realise': [0, 1, -1, 0, 1, -1],
-        'HeurePremiereBaliseActive_realise': [900, 3160, 0, np.nan, np.nan, np.nan],
-        'HeurePremiereBaliseActive_final': [np.nan, np.nan, np.nan, 1440, 2880, -1440],
-        'HeurePremiereBalise_final': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-        'dateRelative_final': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-        'expected': [900, 1720, 1440, 1440, 1440, 0]
-    }
-    df = pd.DataFrame(data)
+    output = read_and_process_file("RDVC-20230522.pln")
+
+    df = pd.DataFrame(output)
     df['heure_de_reference'] = df.apply(calcul_HeureDeReference, axis=1)
     
     for _, row in df.iterrows():
