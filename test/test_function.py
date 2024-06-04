9b1d962c6b3c411735545711760baedc19acbbbb
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-04 10:11:09 +0200

    Update test_function.py

--- test/test_function.py
+++ test/test_function.py
@@ -1,10 +1,10 @@
 # test_script.py
 
 import pytest
-from src.stanToCSV import read_and_process_file, convert_and_calculate, filter_and_analyze
+from src.stanToCSV import read_and_process_file, convert_and_calculate
 
 def test_heure_de_reference():
-    output = read_and_process_file("plan_de_vol.txt")
+    output = read_and_process_file("RDVC-20230522.pln")
     output = convert_and_calculate(output)
     # Assuming filter_and_analyze has been applied and output DataFrame is filtered
     heure_de_reference_ein545 = output.loc[output['callSign_prevu'] == 'EIN545', 'heure_de_reference'].values[0]
