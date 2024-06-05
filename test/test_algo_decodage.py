b9d6bcb3587b2337a3f330e2a4a23673a6be70cf
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-05 19:44:24 +0200

    Update test_algo_decodage.py

--- test/test_algo_decodage.py
+++ test/test_algo_decodage.py
@@ -86,25 +86,25 @@ def test_dateRelative_final_HeurePremiereBalise_final_veille():
     
 def test_dateRelative_realise_jourdarchive():
     date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
-    assert date_de_reference == datetime.datetime(2023, 5, 22, 0, 0), "date_de_reference for EFW2865 is not equal to '2023-05-22'"
+    assert date_de_reference == datetime(2023, 5, 22, 0, 0), "date_de_reference for EFW2865 is not equal to '2023-05-22'"
 
 def test_dateRelative_realise_lendemain():
     date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
-    assert date_de_reference == datetime.datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-22'"
+    assert date_de_reference == datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-22'"
 
 def test_dateRelative_realise_veille():
     date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
-    assert date_de_reference == datetime.datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-22'"
+    assert date_de_reference == datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-22'"
 
 def test_dateRelative_final_jourdarchive():
     date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
-    assert date_de_reference == datetime.datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-22'"
+    assert date_de_reference == datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-22'"
 
 def test_dateRelative_final_lendemain():
     date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
-    assert date_de_reference == datetime.datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-21'"
+    assert date_de_reference == datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-21'"
 
 def test_dateRelative_final_veille():
     date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
-    assert date_de_reference == datetime.datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-23'"
+    assert date_de_reference == datetime(2023, 5, 22, 0, 0), "date_de_reference for 160B is not equal to '2023-05-23'"
 
