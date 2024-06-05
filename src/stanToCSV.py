7b68c43df325d1dcd8fc3d3aee8feb61f2a36156
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-05 19:10:02 +0200

    Update stanToCSV.py

--- src/stanToCSV.py
+++ src/stanToCSV.py
@@ -200,24 +200,25 @@ def convert_and_calculate(df):
 
     df['heure_de_reference'] = df.apply(calcul_HeureDeReference, axis=1)
     
-def calcul_DateDeReference(row):
-    try:
-        if not pd.isna(row['dateRelative_realise']) and not pd.isnull(row['dateRelative_realise']):
-            if row['dateRelative_realise'] == 0:
-                return date_obj
-            elif row['dateRelative_realise'] == 1:
-                return date_obj - timedelta(days=1)
-            elif row['dateRelative_realise'] == -1:
-                return date_obj + timedelta(days=1)
-        elif not pd.isna(row['dateRelative_final']) and not pd.isnull(row['dateRelative_final']):
-            if row['dateRelative_final'] == 0:
-                return date_obj
-            elif row['dateRelative_final'] == 1:
-                return date_obj - timedelta(days=1)
-            elif row['dateRelative_final'] == -1:
-                return date_obj + timedelta(days=1)
-    except Exception as e:
-        return None
+    def calcul_DateDeReference(row):
+        try:
+            if not pd.isna(row['dateRelative_realise']) and not pd.isnull(row['dateRelative_realise']):
+                if row['dateRelative_realise'] == 0:
+                    return date_obj
+                elif row['dateRelative_realise'] == 1:
+                    return date_obj - timedelta(days=1)
+                elif row['dateRelative_realise'] == -1:
+                    return date_obj + timedelta(days=1)
+            elif not pd.isna(row['dateRelative_final']) and not pd.isnull(row['dateRelative_final']):
+                if row['dateRelative_final'] == 0:
+                    return date_obj
+                elif row['dateRelative_final'] == 1:
+                    return date_obj - timedelta(days=1)
+                elif row['dateRelative_final'] == -1:
+                    return date_obj + timedelta(days=1)
+        except Exception as e:
+            return None
+        df['date_de_reference'] = df.apply(calcul_DateeDeReference, axis=1)
 
 
 def filter_and_analyze(df):
