76b8bf82e13d5a03dafef7814177a0b0d966820c
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-05 19:10:18 +0200

    Update test_algo_decodage.py

--- test/test_algo_decodage.py
+++ test/test_algo_decodage.py
@@ -2,11 +2,10 @@
 import pandas as pd
 import numpy as np
 import pytest
-from src.stanToCSV import read_and_process_file, convert_and_calculate, calcul_DateDeReference
+from src.stanToCSV import read_and_process_file, convert_and_calculate
 
 output = read_and_process_file("RDVC-20230522.pln")
 output = convert_and_calculate(output)
-output['date_de_reference'] = output.apply(calcul_DateDeReference, axis=1)
 print(output.info())
 
 def test_dateRelative_realise_HeurePremiereBaliseActive_realise_jourdarchive():
