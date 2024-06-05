25c013ab40a1164fdbe5429e52c9c2aefb5b1263
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-05 22:22:49 +0200

    Update test_algo_decodage.py

--- test/test_algo_decodage.py
+++ test/test_algo_decodage.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 import pytest
 from datetime import datetime, timedelta
-from src.stanToCSV import read_and_process_file, convert_and_calculate
+from src.stanToCSV import read_and_process_file, convert_and_calculate, calcul_HeureDeReference
+
 
 output = read_and_process_file("RDVC-20230522.pln")
 output = convert_and_calculate(output)
 date_str = '22-05-2023'
 date_obj = datetime.strptime(date_str, "%d-%m-%Y")
+output['date_de_reference'] = output.apply(calcul_DateDeReference, axis=1)
 def test_dateRelative_realise_HeurePremiereBaliseActive_realise_jourdarchive():
     heure_de_reference = output.loc[output['callSign_prevu'] == 'SWR9G', 'heure_de_reference'].values[0]
     assert heure_de_reference == 465.0, "heure_de_reference for SWR9G is not equal to 465.0"
