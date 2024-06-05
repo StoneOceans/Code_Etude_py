99d55e2150c61e6493143917e42c778510104bce
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-05 18:13:50 +0200

    Update test_function.py

--- test/test_function.py
+++ test/test_function.py
@@ -83,23 +83,23 @@ def test_dateRelative_final_HeurePremiereBalise_final_veille():
     heure_de_reference = output.loc[output['callSign_prevu'] == 'EFW2865', 'heure_de_reference'].values[0]
     assert heure_de_reference == 2665.0, "heure_de_reference for EFW2865 is not equal to 2665.0"
 def test_dateRelative_realise_jourdarchive():
-    heure_de_reference = output.loc[output['callSign_prevu'] == 'EFW2865', 'heure_de_reference'].values[0]
-    assert heure_de_reference == 2665.0, "heure_de_reference for EFW2865 is not equal to 2665.0"
+    heure_de_reference = output.loc[output['callSign_prevu'] == 'EFW2865', 'date_de_reference'].values[0]
+    assert date_de_reference == '2023-05-22', "date_de_reference for EFW2865 is not equal to 2665.0"
 
 def test_dateRelative_realise_lendemain():
-    heure_de_reference = output.loc[output['callSign_prevu'] == 'EFW2865', 'heure_de_reference'].values[0]
-    assert heure_de_reference == 2665.0, "heure_de_reference for EFW2865 is not equal to 2665.0"
+    date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
+    assert date_de_reference == '2023-05-22', "date_de_reference for 160B is not equal to '2023-05-22'"
 def test_dateRelative_realise_veille():
-    heure_de_reference = output.loc[output['callSign_prevu'] == 'EFW2865', 'heure_de_reference'].values[0]
-    assert heure_de_reference == 2665.0, "heure_de_reference for EFW2865 is not equal to 2665.0"
+    date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
+    assert date_de_reference == '2023-05-22', "date_de_reference for 160B is not equal to '2023-05-22'"
 
 def test_dateRelative_final_jourdarchive():
-    heure_de_reference = output.loc[output['callSign_prevu'] == 'EFW2865', 'heure_de_reference'].values[0]
-    assert heure_de_reference == 2665.0, "heure_de_reference for EFW2865 is not equal to 2665.0"
+    date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
+    assert date_de_reference == '2023-05-22', "date_de_reference for 160B is not equal to '2023-05-22'"
 
 def test_dateRelative_final_lendemain():
-    heure_de_reference = output.loc[output['callSign_prevu'] == 'EFW2865', 'heure_de_reference'].values[0]
-    assert heure_de_reference == 2665.0, "heure_de_reference for EFW2865 is not equal to 2665.0"
+    date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
+    assert heure_de_reference == '2023-05-22', "heure_de_reference for 160B is not equal to '2023-05-22'"
 def test_dateRelative_final_veille():
-    heure_de_reference = output.loc[output['callSign_prevu'] == 'EFW2865', 'heure_de_reference'].values[0]
-    assert heure_de_reference == 2665.0, "heure_de_reference for EFW2865 is not equal to 2665.0"
+    date_de_reference = output.loc[output['callSign_prevu'] == '160B', 'date_de_reference'].values[0]
+    assert date_de_reference == '2023-05-22', "date_de_reference for 160B is not equal to '2023-05-22'"
