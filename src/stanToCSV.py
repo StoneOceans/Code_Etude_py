ee4e536d0f4926cd2de77b73b4a743af41053a9e
Ajar Latti <83123414+StoneOceans@users.noreply.github.com>
2024-06-06 12:01:24 +0200

    Update stanToCSV.py

--- src/stanToCSV.py
+++ src/stanToCSV.py
@@ -3,33 +3,21 @@ import numpy as np
 from datetime import datetime, timedelta
 
 def read_and_process_file(fichier_a_deposee):
-    vol_prevu = []
-    vol_fini = []
-    vol_termine = []
-    tableau_vol = {}
-    tableaux_vol = []
-    iter = 0
-    flag82 = False
-    hneg = False
-    compt82 = 0
-    num81 = 0
-    prevu = False
-    termine = False
-    final = False
-    complet = 0
     output = pd.DataFrame()
-    compteur = 0
     tableau_vol = {}
-    compteurCcr = 0
-
+    
     with open(fichier_a_deposee, 'r') as fichier:
-        for i, ligne in enumerate(fichier):
+        for ligne in fichier:
             words = ligne.split()
+            if not words:
+                continue
+
             if words[0] == "02":
                 date_str = words[1]
                 date_obj = datetime.strptime(date_str, "%d-%m-%Y")
                 date_fichier = date_obj.timetuple().tm_yday
-            if words[0] == "05":
+
+            elif words[0] == "05":
                 if tableau_vol:
                     tableau_vol["isPrevu"] = isprevu
                     tableau_vol["isRealise"] = isrealise
@@ -37,140 +25,140 @@ def read_and_process_file(fichier_a_deposee):
                     df_dictionary = pd.DataFrame([tableau_vol])
                     output = pd.concat([output, df_dictionary], ignore_index=True)
                 tableau_vol = {}
-                isprevu = False
-                isrealise = False
-                isfinal = False
-                balise = ''
-                hneg = False
-                flag82 = False
-                etat = ''
-            if words[0] == "11":
+                isprevu, isrealise, isfinal = False, False, False
+
+            elif words[0] == "11":
                 etat = 'prevu'
                 isprevu = True
-            if words[0] == "12":
+
+            elif words[0] == "12":
                 etat = 'realise'
                 isrealise = True
-            if words[0] == "13":
+
+            elif words[0] == "13":
                 if len(words) > 1 and words[1] == "=":
                     for key in list(tableau_vol.keys()):
                         if '_prevu' in key:
                             tableau_vol[key.replace('_prevu', '_final')] = tableau_vol[key]
                 etat = 'final'
                 isfinal = True
-            if words[0] == "14":
-                etat = 'transaction'
-            if words[0] == "81":
-                pass
-            if words[0] == "20":
-                tableau_vol['callSign_' + etat] = words[1]
-                tableau_vol['dep_' + etat] = words[2]
-                tableau_vol['arr_' + etat] = words[3]
-                tableau_vol['numCautra_' + etat] = words[4]
-                tableau_vol['dateRelative_' + etat] = words[5]
-                tableau_vol['typeAvion_' + etat] = words[6]
-                tableau_vol['work_' + etat] = words[7]
-                if words[8][:2] == '??':
-                    pass
-                else:
-                    tableau_vol['work1' + etat] = words[8].strip().ljust(9)
-            if words[0] == "21":
-                tableau_vol['heuresDep_' + etat] = words[1]
-                tableau_vol['RFL_' + etat] = words[2]
-                tableau_vol['vitesse_' + etat] = words[3]
-                tableau_vol['EOBT_' + etat] = words[4]
-            if words[0] == "22":
-                tableau_vol['regleVol_' + etat] = words[1]
-                tableau_vol['typeVol_' + etat] = words[2]
-                tableau_vol['HeurePremiereBaliseActive_' + etat] = words[10]
-                if words[3][:2] == '??':
-                    pass
-                else:
-                    tableau_vol['IFPL_' + etat] = words[3].strip().ljust(10)
-                tableau_vol['plnActive_' + etat] = words[4]
-                tableau_vol['plnAnnule_' + etat] = words[5]
-                if '??' in words[7]:
-                    pass
-                elif len(words[7]) == 8:
-                    date_str = words[7]
-                    date_obj = datetime.strptime(date_str, '%d%m%Y')
-                    day_vol = date_obj.timetuple().tm_yday
-                    tableau_vol['dateBlock_' + etat] = words[7][:4] + words[7][6:]
-                else:
-                    tableau_vol['dateBlock_' + etat] = words[7].strip().ljust(6)
-            if words[0] == "23":
-                if "??" in words[4]:
-                    tableau_vol['adresseModeS_' + etat] = np.NaN
-                else:
-                    tableau_vol['adresseModeS_' + etat] = words[4]
-            if words[0] == "24":
-                tableau_vol['numeroPLNM' + etat] = words[1]
-                tableau_vol['flightID' + etat] = words[2]
-            if words[0] == "31":
-                tableau_vol['balise' + etat] = words[1]
-            if words[0] == "32":
-                tableau_vol['HeurePremiereBalise_' + etat] = words[1]
-            if words[0] == "33":
-                tableau_vol['listeBalises' + etat] = words[1]
-            if words[0] == "36":
-                tableau_vol['indicateur' + etat] = words[1]
-            if words[0] == "41":
-                tableau_vol['carte' + etat] = words[1]
-            if words[0] == "71":
-                tableau_vol['centreTraversé' + etat] = words[1]
-            if words[0] == "72":
-                tableau_vol['listeRangPremier' + etat] = words[1]
-            if words[0] == "80":
-                tableau_vol['rangTransaction' + etat] = words[1]
-            if words[0] == "81":
-                if len(words) >= 15:
-                    parts = ligne.split("-")
-                    last_word = parts[0].split()[-1]
-                    if "ABI" in ligne:
-                        tableau_vol['typePln'] = "ABI"
-                    if "(FPL" in parts[0] or "(CHG)" in parts[0]:
-                        tableau_vol['case7'] = parts[1]
-                        tableau_vol['case8'] = parts[2]
-                        tableau_vol['case9'] = parts[3]
-                        tableau_vol['case10'] = parts[4]
-                        tableau_vol['case13'] = parts[5]
-                        tableau_vol['case15'] = parts[6]
-                        if len(parts) > 8:
-                            tableau_vol['case16'] = parts[7]
-                            tableau_vol['case18'] = parts[8]
-                            if tableau_vol['case18'] == "RPL":
-                                tableau_vol['typePln'] = "RPL"
-                        else:
-                            print(ligne)
-                            compteur += 1
-                    elif "(APL" in parts[0]:
-                        tableau_vol['case7'] = parts[1]
-                        tableau_vol['case8'] = parts[2]
-                        tableau_vol['case9'] = parts[3]
-                        tableau_vol['case10'] = parts[4]
-                        tableau_vol['case13'] = parts[5]
-                        tableau_vol['case15'] = parts[6]
-                        tableau_vol['typePln'] = "APL"
-                        if len(parts) > 8:
-                            tableau_vol['case16'] = parts[7]
-                            tableau_vol['case18'] = parts[-1]
-                        else:
-                            print(ligne)
-                            compteur += 1
-            if words[0] == "82":
-                tableau_vol['heure'] = (words[1][:2])
-                tableau_vol['minute'] = (words[1][3:])
-                tableau_vol['accuseTrt' + etat] = words[1]
-                if "CCR:" in ligne:
-                    compteurCcr = 0
-                    for word in words:
-                        compteurCcr += 1
-                        if word == "CCR:":
-                            break
-                    tableau_vol['ccrArrival'] = words[compteurCcr]
-            if words[0] == "84":
-                tableau_vol['final' + etat] = words[1]
+
+            elif words[0] in ["14", "20", "21", "22", "23", "24", "31", "32", "33", "36", "41", "71", "72", "80", "81", "82", "84"]:
+                tableau_vol = process_lines(words, ligne, tableau_vol, etat)
 
     return output
+
+def process_lines(words, ligne, tableau_vol, etat):
+    if words[0] == "20":
+        tableau_vol['callSign_' + etat] = words[1]
+        tableau_vol['dep_' + etat] = words[2]
+        tableau_vol['arr_' + etat] = words[3]
+        tableau_vol['numCautra_' + etat] = words[4]
+        tableau_vol['dateRelative_' + etat] = words[5]
+        tableau_vol['typeAvion_' + etat] = words[6]
+        tableau_vol['work_' + etat] = words[7]
+        tableau_vol['work1_' + etat] = words[8].strip().ljust(9) if words[8][:2] != '??' else None
+
+    elif words[0] == "21":
+        tableau_vol['heuresDep_' + etat] = words[1]
+        tableau_vol['RFL_' + etat] = words[2]
+        tableau_vol['vitesse_' + etat] = words[3]
+        tableau_vol['EOBT_' + etat] = words[4]
+
+    elif words[0] == "22":
+        tableau_vol['regleVol_' + etat] = words[1]
+        tableau_vol['typeVol_' + etat] = words[2]
+        tableau_vol['HeurePremiereBaliseActive_' + etat] = words[10]
+        tableau_vol['IFPL_' + etat] = words[3].strip().ljust(10) if words[3][:2] != '??' else None
+        tableau_vol['plnActive_' + etat] = words[4]
+        tableau_vol['plnAnnule_' + etat] = words[5]
+        tableau_vol['dateBlock_' + etat] = parse_date_block(words[7])
+
+    elif words[0] == "23":
+        tableau_vol['adresseModeS_' + etat] = words[4] if "??" not in words[4] else np.NaN
+
+    elif words[0] == "24":
+        tableau_vol['numeroPLNM_' + etat] = words[1]
+        tableau_vol['flightID_' + etat] = words[2]
+
+    elif words[0] == "31":
+        tableau_vol['balise_' + etat] = words[1]
+
+    elif words[0] == "32":
+        tableau_vol['HeurePremiereBalise_' + etat] = words[1]
+
+    elif words[0] == "33":
+        tableau_vol['listeBalises_' + etat] = words[1]
+
+    elif words[0] == "36":
+        tableau_vol['indicateur_' + etat] = words[1]
+
+    elif words[0] == "41":
+        tableau_vol['carte_' + etat] = words[1]
+
+    elif words[0] == "71":
+        tableau_vol['centreTraverse_' + etat] = words[1]
+
+    elif words[0] == "72":
+        tableau_vol['listeRangPremier_' + etat] = words[1]
+
+    elif words[0] == "80":
+        tableau_vol['rangTransaction_' + etat] = words[1]
+
+    elif words[0] == "81":
+        tableau_vol = process_case_81(ligne, tableau_vol)
+
+    elif words[0] == "82":
+        tableau_vol['heure'] = (words[1][:2])
+        tableau_vol['minute'] = (words[1][3:])
+        tableau_vol['accuseTrt_' + etat] = words[1]
+        if "CCR:" in ligne:
+            tableau_vol['ccrArrival'] = words[words.index("CCR:") + 1]
+
+    elif words[0] == "84":
+        tableau_vol['final_' + etat] = words[1]
+
+    return tableau_vol
+
+def process_case_81(ligne, tableau_vol):
+    parts = ligne.split("-")
+    last_word = parts[0].split()[-1]
+    if "ABI" in ligne:
+        tableau_vol['typePln'] = "ABI"
+    if "(FPL" in parts[0] or "(CHG)" in parts[0]:
+        tableau_vol['case7'] = parts[1]
+        tableau_vol['case8'] = parts[2]
+        tableau_vol['case9'] = parts[3]
+        tableau_vol['case10'] = parts[4]
+        tableau_vol['case13'] = parts[5]
+        tableau_vol['case15'] = parts[6]
+        if len(parts) > 8:
+            tableau_vol['case16'] = parts[7]
+            tableau_vol['case18'] = parts[8]
+            if tableau_vol['case18'] == "RPL":
+                tableau_vol['typePln'] = "RPL"
+    elif "(APL" in parts[0]:
+        tableau_vol['case7'] = parts[1]
+        tableau_vol['case8'] = parts[2]
+        tableau_vol['case9'] = parts[3]
+        tableau_vol['case10'] = parts[4]
+        tableau_vol['case13'] = parts[5]
+        tableau_vol['case15'] = parts[6]
+        tableau_vol['typePln'] = "APL"
+        if len(parts) > 8:
+            tableau_vol['case16'] = parts[7]
+            tableau_vol['case18'] = parts[-1]
+
+    return tableau_vol
+
+def parse_date_block(date_str):
+    if '??' in date_str:
+        return None
+    elif len(date_str) == 8:
+        date_obj = datetime.strptime(date_str, '%d%m%Y')
+        return date_obj.timetuple().tm_yday
+    else:
+        return date_str.strip().ljust(6)
 
 def convert_and_calculate(df):
     df['HeurePremiereBaliseActive_realise'] = df['HeurePremiereBaliseActive_realise'].astype('Int64')
@@ -181,42 +169,28 @@ def convert_and_calculate(df):
 
     def calcul_HeureDeReference(row):
         try:
-            if not pd.isna(row['dateRelative_realise']):
-                if not pd.isna(row['HeurePremiereBaliseActive_realise']):
-                    return int(row['HeurePremiereBaliseActive_realise']) + (1440 if row['dateRelative_realise'] == -1 else -1440 if row['dateRelative_realise'] == 1 else 0)
-                elif not pd.isna(row['HeurePremiereBaliseActive_final']):
-                    return int(row['HeurePremiereBaliseActive_final']) + (1440 if row['dateRelative_realise'] == -1 else -1440 if row['dateRelative_realise'] == 1 else 0)
-                elif not pd.isna(row['HeurePremiereBalise_final']):
-                    return int(row['HeurePremiereBalise_final']) + (1440 if row['dateRelative_realise'] == -1 else -1440 if row['dateRelative_realise'] == 1 else 0)
-            elif not pd.isna(row['dateRelative_final']):
-                if not pd.isna(row['HeurePremiereBaliseActive_realise']):
-                    return int(row['HeurePremiereBaliseActive_realise']) + (1440 if row['dateRelative_final'] == -1 else -1440 if row['dateRelative_final'] == 1 else 0)
-                elif not pd.isna(row['HeurePremiereBaliseActive_final']):
-                    return int(row['HeurePremiereBaliseActive_final']) + (1440 if row['dateRelative_final'] == -1 else -1440 if row['dateRelative_final'] == 1 else 0)
-                elif not pd.isna(row['HeurePremiereBalise_final']):
-                    return int(row['HeurePremiereBalise_final']) + (1440 if row['dateRelative_final'] == -1 else -1440 if row['dateRelative_final'] == 1 else 0)
+            date_relative = row['dateRelative_realise'] if not pd.isna(row['dateRelative_realise']) else row['dateRelative_final']
+            if not pd.isna(date_relative):
+                heure_active = row['HeurePremiereBaliseActive_realise'] if not pd.isna(row['HeurePremiereBaliseActive_realise']) else \
+                    row['HeurePremiereBaliseActive_final'] if not pd.isna(row['HeurePremiereBaliseActive_final']) else \
+                    row['HeurePremiereBalise_final']
+                if not pd.isna(heure_active):
+                    return int(heure_active) + (1440 if date_relative == -1 else -1440 if date_relative == 1 else 0)
         except Exception:
             return None
 
     df['heure_de_reference'] = df.apply(calcul_HeureDeReference, axis=1)
-    
+
     def calcul_DateDeReference(row):
         try:
-          if not pd.isna(row['dateRelative_realise']) and not pd.isnull(row['dateRelative_realise']):
-              if row['dateRelative_realise'] == 0:
-                        return pd.Timestamp(date_obj)
-              elif row['dateRelative_realise'] == 1:
-                        return pd.Timestamp(date_obj - timedelta(days=1))
-              elif row['dateRelative_realise'] == -1:
-                        return pd.Timestamp(date_obj + timedelta(days=1))
-          elif not pd.isna(row['dateRelative_final']) and not pd.isnull(row['dateRelative_final']):
-              if row['dateRelative_final'] == 0:
-                        return pd.Timestamp(date_obj)
-              elif row['dateRelative_final'] == 1:
-                        return pd.Timestamp(date_obj - timedelta(days=1))
-              elif row['dateRelative_final'] == -1:
-                        return pd.Timestamp(date_obj + timedelta(days=1))
-    
+            date_relative = row['dateRelative_realise'] if not pd.isna(row['dateRelative_realise']) else row['dateRelative_final']
+            if not pd.isna(date_relative):
+                if date_relative == 0:
+                    return pd.Timestamp(date_obj)
+                elif date_relative == 1:
+                    return pd.Timestamp(date_obj - timedelta(days=1))
+                elif date_relative == -1:
+                    return pd.Timestamp(date_obj + timedelta(days=1))
         except Exception as e:
             return None
 
@@ -226,7 +200,7 @@ def filter_and_analyze(df):
     df_filtre = df.dropna(subset=['heure_de_reference']).copy()
     df_filtre['transmission'] = df_filtre.apply(
         lambda row: 'ABI' if not pd.isna(row['case7']) else ('RPL' if row['typePln'] == 'RPL' else 'AUTRE'), axis=1)
-    
+
     transmissions_count = df_filtre['transmission'].value_counts()
     df_filtre.sort_values(by=['heure_de_reference'], inplace=True)
     return df_filtre, transmissions_count
